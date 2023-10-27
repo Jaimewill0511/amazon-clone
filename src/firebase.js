5b8952a7449263dba3b13fe4c6e92dcf97cd9f40
William Binitie <binitiewilliam0511@gmail.com>
2023-10-27 07:58:18 -0400

    Init app

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBaFNDAMYe8MHeKw1oSWmLiT3rUmz8Ky-c",
+    authDomain: "clone-eab71.firebaseapp.com",
+    projectId: "clone-eab71",
+    storageBucket: "clone-eab71.appspot.com",
+    messagingSenderId: "175001446154",
+    appId: "1:175001446154:web:08f95b10157873f478fcf3",
+    measurementId: "G-RDRFJX8FME"
+  };  
